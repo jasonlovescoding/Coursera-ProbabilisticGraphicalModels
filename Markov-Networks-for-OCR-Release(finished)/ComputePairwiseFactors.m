14db7757c81bb4d76dcd47602577fbf7797a2a8e
Jason ZHANG <zhangqianhao1408@gmail.com>
2017-02-17 12:28:56 +0800

    Part I representation

---
+++ Markov-Networks-for-OCR-Release(finished)/ComputePairwiseFactors.m
@@ -0,0 +1,40 @@
+function factors = ComputePairwiseFactors (images, pairwiseModel, K)
+% This function computes the pairwise factors for one word and uses the
+% given pairwise model to set the factor values.
+%
+% Input:
+%   images: An array of structs containing the 'img' value for each
+%     character in the word.
+%   pairwiseModel: The provided pairwise model. It is a K-by-K matrix. For
+%     character i followed by character j, the factor value should be
+%     pairwiseModel(i, j).
+%   K: The alphabet size (accessible in imageModel.K for the provided
+%     imageModel).
+%
+% Output:
+%   factors: The pairwise factors for this word.
+%
+% Copyright (C) Daphne Koller, Stanford University, 2012
+
+n = length(images);
+
+% If there are fewer than 2 characters, return an empty factor list.
+if (n < 2)
+    factors = [];
+    return;
+end
+
+factors = repmat(struct('var', [], 'card', [], 'val', []), n - 1, 1);
+
+% Your code here:
+for i = 1:(n-1)
+    factors(i).var = [i, i+1];
+    factors(i).card = [K, K];
+    for c1 = 1:K
+        for c2 = 1:K
+            idx = AssignmentToIndex([c1, c2], factors(i).card);
+            factors(i).val(idx) = pairwiseModel(c1, c2);
+        end
+    end
+end
+end
---
+++ Markov-Networks-for-OCR-Release(finished)/ComputeSimilarityFactor.m
@@ -0,0 +1,37 @@
+function factor = ComputeSimilarityFactor (images, K, i, j)
+% This function computes the similarity factor between two character images
+% in one word --- which characters is given by indices i and j (a
+% description of how the factor should be computed is given below).
+%
+% Input:
+%   images: A struct array of character images from one word.
+%   K: The alphabet size.
+%   i,j: The scope of that factor. That is, you should construct a factor
+%     between characters i and j in the images array.
+%
+% Output:
+%   factor: The similarity factor between these two characters. For any
+%     assignment C_i != C_j, the factor value should be one. For any
+%     assignment C_i == C_j, the factor value should be
+%     ImageSimilarity(I_i, I_j) --- ie, the computed value given by
+%     ImageSimilarity.m on the two images.
+%
+% Copyright (C) Daphne Koller, Stanford University, 2012
+
+factor = struct('var', [], 'card', [], 'val', []);
+
+% Your code here:
+factor.var = [i, j];
+factor.card = [K, K];
+for c1 = 1:K
+    for c2 = 1:K
+        idx = AssignmentToIndex([c1, c2], factor.card);
+        if (c1 == c2)
+            factor.val(idx) = ImageSimilarity(images(i).img, images(j).img); 
+        else
+            factor.val(idx) = 1; 
+        end
+    end
+end    
+end
+
