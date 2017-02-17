2610061996fdf6884eae78c06a2b19ff624e1a8a
Jason ZHANG <zhangqianhao1408@gmail.com>
2017-02-17 12:35:57 +0800

    Part II inference

---
+++ Sampling-Methods-Release(finished)/MHUniformTrans.m
@@ -0,0 +1,37 @@
+% MHUNIFORMTRANS
+%
+%  MCMC Metropolis-Hastings transition function that
+%  utilizes the uniform proposal distribution.
+%  A - The current joint assignment.  This should be
+%      updated to be the next assignment
+%  G - The network
+%  F - List of all factors
+%
+% Copyright (C) Daphne Koller, Stanford University, 2012
+
+function A = MHUniformTrans(A, G, F)
+
+% Draw proposed new state from uniform distribution
+A_prop = ceil(rand(1, length(A)) .* G.card);
+
+p_acceptance = 0.0;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% YOUR CODE HERE
+% Compute acceptance probability
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+forward = 1.0;
+backward = 1.0;
+
+for i = 1:length(F),
+  forward = forward * GetValueOfAssignment(F(i), A_prop(F(i).var) );
+  backward = backward * GetValueOfAssignment(F(i), A(F(i).var) );
+end
+
+p_acceptance = min([1 (forward/backward)]);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Accept or reject proposal
+if rand() < p_acceptance
+    % disp('Accepted');
+    A = A_prop;
+end
